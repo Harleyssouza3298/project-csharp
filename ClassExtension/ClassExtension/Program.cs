98310eda5abdc6a6e9e06a3e98c7414274c76822
Harley Souza <harleyssouza@gmail.com>
2023-11-23 18:46:14 +0000

    Create a DateTime extension

---
+++ ClassExtension/ClassExtension/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassExtension.Extensions;
+using System.Data;
+
+namespace ClassExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 11, 23, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
