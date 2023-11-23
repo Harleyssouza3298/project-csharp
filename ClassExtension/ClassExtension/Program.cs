2b3602bb62eaffe6248e66b13ee87506d6a2ac75
Harley Souza <harleyssouza@gmail.com>
2023-11-23 18:57:45 +0000

    Create a String Extension

--- ClassExtension/ClassExtension/Program.cs
+++ ClassExtension/ClassExtension/Program.cs
@@ -8,7 +8,17 @@ namespace ClassExtension
         static void Main(string[] args)
         {
             DateTime dt = new DateTime(2023, 11, 23, 8, 10, 45);
+
+            Console.WriteLine("----------DateTime Extension-------------------");
             Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine();
+            Console.WriteLine("----------String Extension-------------------");
+      
+            string s1 = "Good morning dear students!";
+
+            Console.WriteLine("Original string: " + s1);
+            Console.WriteLine("Using Extension: " + s1.Cut(10)) ;
         }
     }
 }
